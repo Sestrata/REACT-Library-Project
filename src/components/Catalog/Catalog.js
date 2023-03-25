e89eebfec7c85667f98fc3de1fe7affaf52a8733
Elena Yaneva <elena_yaneva_@abv.bg>
2023-03-25 18:30:31 +0200

    searchbar

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,17 +1,15 @@
 import { CatalogItem } from "./CatalogItem/CatalogItem";
-// import { Search } from "../Search/Search";
+import { Search } from "../Search/Search";
 
 export const Catalog = ({
     books,
 }) => {
     return (
         <section className="catalogPage">
+            <Search placeholder="Search..." data={books}/>
             <h1>All books</h1>
-
-            {/* <Search /> */}
-
             <ul>
-                {books.map(x => <CatalogItem key={x._id} {...x}  />)}
+                {books.map(x => <CatalogItem key={x._id} {...x} />)}
             </ul>
             {books.length === 0 && (<h3 className="no-books">No books yet</h3>)}
         </section>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,59 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-// export const Search = () => {
-//     return (
-//         <section className="search-container">
-//             <form>
-//                 <input type="text" placeholder="Search.." name="search" />
-//                 <button type="submit"><i className="fas fa-search"></i></button>
-//             </form>
-//         </section>
-//     )
-// }
+export const Search = ({
+    placeholder,
+    data,
+}) => {
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleFilter = (e) => {
+        const searchWord = e.target.value;
+        setWordEntered(searchWord);
+        const newFilter = data.filter((value) => {
+            return value.bookName.toLowerCase().includes(searchWord.toLowerCase())
+                || value.author.toLowerCase().includes(searchWord.toLowerCase())
+                || value.ganre.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if (searchWord === '') {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        };
+    };
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+    }
+
+    return (
+        <div className="search-container">
+            <div className="searchInputs">
+                <input
+                    type="text"
+                    value={wordEntered}
+                    placeholder={placeholder}
+                    onChange={handleFilter}
+                />
+                <button type="submit" className="searchIcon">
+                    {filteredData.length === 0
+                        ? <i className="fas fa-search"></i>
+                        : <i className="fas fa-times" onClick={clearInput}></i>}
+                </button>
+            </div>
+
+            {filteredData.length !== 0 && (
+                <div className="dataResult">
+                    {filteredData.slice(0, 10).map((value, i) => {
+                        return <Link to={value._id} key={i} className="dataItem" target="_blank">
+                            <p>{value.bookName || value.author || value.ganre}</p>
+                        </Link>
+                    })}
+                </div>
+            )}
+        </div>
+    );
+}
